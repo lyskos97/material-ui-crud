f666b318d532b28739fdc928a0ce4b162e5b719d
Konstantin Lyssenko <lyskos97@gmail.com>
2018-05-26 17:20:45 +0600

    feat: About page placeholder

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography, Paper, Divider } from '@material-ui/core';
+
+export default () => (
+  <React.Fragment>
+    <Typography variant="display1">About</Typography>
+    <Divider />
+    <Typography variant="body2">Material-UI based React App</Typography>
+  </React.Fragment>
+);
