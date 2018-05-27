268e8f1e3d8bae35bde9f78d59829c5a160fac7b
Konstantin Lyssenko <lyskos97@gmail.com>
2018-05-27 20:50:35 +0600

    feat: addNote form dialog

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,52 +1,182 @@
 import * as React from 'react';
-import { GridList, Grid, Typography } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  CircularProgress,
+  Button,
+  Icon,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  InputLabel,
+  Input,
+  FormControl
+} from '@material-ui/core';
+import axios from 'axios';
 
 import Note from './Note';
 
-const mockNotes = [
-  { id: 1, title: 'Note #1', text: 'Do this and that, so I could get ', updatedAt: new Date() },
-  { id: 2, title: 'Note #2', text: 'That, so I could get those', updatedAt: new Date() },
-  { id: 3, title: 'Note #3', text: 'this, that, so I could get dat', updatedAt: new Date() },
-  { id: 4, title: 'Note #4', text: 'Do this and that, those', updatedAt: new Date() },
-  { id: 5, title: 'Note #5', text: 'Nope,this and that, so get those', updatedAt: new Date() },
-  { id: 6, title: 'Note #6', text: 'Kinda that, so I could get those', updatedAt: new Date() },
-  { id: 7, title: 'Note #7', text: 'Props, and that, so I could get those', updatedAt: new Date() }
-];
+const styles = {
+  formField: {
+    margin: '10px 0'
+  }
+};
 
 class Notes extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      notes: mockNotes,
+      openDialog: false,
+      newNote: {
+        title: '',
+        text: ''
+      },
       loading: false,
-      error: false
+      error: false,
+      notes: []
     };
   }
 
-  deleteNote = idToDelete => {
-    const updatedNotes = this.state.notes.filter(({ id }) => id !== idToDelete);
-    this.setState({ notes: updatedNotes });
+  async componentDidMount() {
+    await this.fetchNotes();
+  }
+
+  static getDerivedStateFromProps = () => {
+    return null;
+  };
+
+  fetchNotes = () => {
+    console.log('fetching notes...');
+    this.setState({ loading: true }, async () => {
+      const { data } = await axios.get('/notes');
+      console.log('notes', data);
+      this.setState({ notes: data.notes, loading: false });
+    });
+  };
+
+  handleChange = e => {
+    const { newNote } = this.state;
+    const { name, value } = e.target;
+
+    this.setState({ newNote: { ...newNote, [name]: value } });
+  };
+
+  handleDiglogOpen = () => {
+    this.setState({ openDialog: true });
+  };
+
+  handleDialogClose = () => {
+    this.setState({
+      openDialog: false,
+      newNote: {
+        title: '',
+        text: ''
+      }
+    });
+  };
+
+  handleNoteAdd = async () => {
+    const { title, text } = this.state.newNote;
+    try {
+      await axios.post('/notes', { title, text });
+
+      this.fetchNotes();
+      this.setState({
+        openDialog: false,
+        newNote: {
+          title: '',
+          text: ''
+        }
+      });
+    } catch (e) {
+      console.log('addNote arr', e);
+      this.setState({
+        error: true,
+        openDialog: false,
+        newNote: {
+          title: '',
+          text: ''
+        }
+      });
+    }
   };
 
   render() {
-    const { notes } = this.state;
+    const { notes, loading, error, openDialog, newNote } = this.state;
+    const { title, text } = newNote;
+
+    if (error) return 'error';
 
     return (
       <React.Fragment>
         <Typography variant="headline">Your notes</Typography>
         <Grid container spacing={8}>
-          {notes.map(note => (
-            <Grid key={note.id} item md={4} xs={12} sm={6}>
-              <Note
-                note={note}
-                deleteNote={() => {
-                  this.deleteNote(note.id);
-                }}
-              />
-            </Grid>
-          ))}
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            notes.map(note => (
+              <Grid key={note._id} item md={4} xs={12} sm={6}>
+                <Note refetchNotes={this.fetchNotes} note={note} />
+              </Grid>
+            ))
+          )}
         </Grid>
+        {this.props.user && (
+          <React.Fragment>
+            <Button
+              onClick={this.handleDiglogOpen}
+              variant="fab"
+              color="primary"
+              style={{ position: 'fixed', right: 50, bottom: 50 }}
+            >
+              <Icon>add_icon</Icon>
+            </Button>
+            <Dialog open={openDialog} onClose={this.handleDialogClose}>
+              <DialogTitle>New note</DialogTitle>
+              <DialogContent>
+                {/* <DialogContentText>You know</DialogContentText> */}
+                <FormControl style={styles.formField} fullWidth>
+                  <InputLabel htmlFor="add-note-title">Title</InputLabel>
+                  <Input
+                    fullWidth
+                    autoFocus
+                    id="add-note-title"
+                    name="title"
+                    value={title}
+                    onChange={this.handleChange}
+                    endAdornment={<Icon>edit_icon</Icon>}
+                    multiline
+                  />
+                </FormControl>
+                <FormControl style={styles.formField} fullWidth>
+                  <InputLabel htmlFor="add-note-text">Text</InputLabel>
+                  <Input
+                    value={text}
+                    name="text"
+                    id="add-note-text"
+                    onChange={this.handleChange}
+                    endAdornment={<Icon>edit_icon</Icon>}
+                    fullWidth
+                    multiline
+                    rowsMax={10}
+                    rows={3}
+                  />
+                </FormControl>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleDialogClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={this.handleNoteAdd} color="primary">
+                  Save
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </React.Fragment>
+        )}
       </React.Fragment>
     );
   }
