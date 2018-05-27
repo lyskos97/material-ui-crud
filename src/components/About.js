708ad8def4256b7de72c9dc8432a97da075fb993
Konstantin Lyssenko <lyskos97@gmail.com>
2018-05-27 20:53:15 +0600

    feat: disclaimer page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Typography, Paper, Divider } from '@material-ui/core';
+import { Typography, Paper, Divider, List, ListItem } from '@material-ui/core';
 
 export default () => (
   <React.Fragment>
-    <Typography variant="display1">About</Typography>
-    <Divider />
-    <Typography variant="body2">Material-UI based React App</Typography>
+    <Paper style={{ padding: 20 }}>
+      <Typography variant="display1">About</Typography>
+      <Divider style={{ margin: '15px 0' }} />
+      <Typography variant="title" gutterBottom>
+        Material-UI based CRUD React App
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Made by{' '}
+        <a href="https://github.com/lyskos97" target="_blank">
+          @lyskos97
+        </a>{' '}
+        for RESTful APIs with bearer-based auth
+      </Typography>
+      <Typography variant="headline">Featuring</Typography>
+      <List  dense={true}>
+        <ListItem>React Router</ListItem>
+        <ListItem>Axios</ListItem>
+        <ListItem>Webpack</ListItem>
+      </List>
+    </Paper>
   </React.Fragment>
 );
