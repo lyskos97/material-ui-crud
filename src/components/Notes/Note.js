2a1248936b34290743d8cc6d4ae601a8a2af44b8
Konstantin Lyssenko <lyskos97@gmail.com>
2018-05-27 16:21:36 +0600

    feat: local editable note list

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,27 +1,141 @@
 import * as React from 'react';
-import { Paper, Typography, Card, CardContent, CardHeader, Icon, Button } from '@material-ui/core';
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardHeader,
+  Icon,
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  Divider
+} from '@material-ui/core';
 
 const styles = {
-  root: { marginTop: 5, marginBottom: 5 }
+  root: { marginTop: 5, marginBottom: 5 },
+  button: { margin: 5 },
+  header: { paddingBottom: 10 },
+  content: { paddingTop: 5 }
 };
 
 class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      edit: false,
+      note: props.note
+    };
+  }
+
+  handleEdit = () => {
+    this.setState({ edit: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ note: this.props.note, edit: false });
+  };
+
+  handleSave = () => {
+    // TODO:
+    this.setState({ edit: false });
+  };
+
+  handleChange = e => {
+    const { note } = this.state;
+    const { name, value } = e.target;
+
+    this.setState({ note: { ...note, [name]: value } });
+  };
+
   render() {
+    const { edit, note } = this.state;
+    const { title, text, updatedAt } = note;
+
     return (
       <Card style={styles.root}>
         <CardHeader
-          title="Get better with Material-UI"
+          title={
+            edit ? (
+              <FormControl fullWidth>
+                <InputLabel htmlFor="note-title">Title</InputLabel>
+                <Input
+                  fullWidth
+                  id="note-title"
+                  name="title"
+                  value={title}
+                  onChange={this.handleChange}
+                  endAdornment={<Icon>edit_icon</Icon>}
+                  multiline
+                />
+              </FormControl>
+            ) : (
+              title
+            )
+          }
+          style={styles.header}
           action={
-            <Button color="primary" variant="fab">
-              <Icon>edit_icon</Icon>
-            </Button>
+            edit ? (
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Button
+                  onClick={this.handleCancel}
+                  style={styles.button}
+                  color="secondary"
+                  variant="fab"
+                  mini
+                >
+                  <Icon>close_icon</Icon>
+                </Button>
+                <Button
+                  onClick={this.handleSave}
+                  style={styles.button}
+                  mini
+                  variant="fab"
+                  color="primary"
+                >
+                  <Icon>check_icon</Icon>
+                </Button>
+              </div>
+            ) : (
+              <React.Fragment>
+                <Button onClick={this.handleEdit} style={styles.button} variant="fab">
+                  <Icon>edit_icon</Icon>
+                </Button>
+                <Button
+                  onClick={this.props.deleteNote}
+                  style={styles.button}
+                  color="secondary"
+                  variant="fab"
+                >
+                  <Icon>delete_icon</Icon>
+                </Button>
+              </React.Fragment>
+            )
           }
         />
-        <CardContent>
-          <Typography gutterBottom variant="body1" component="p" color="textSecondary">
-            Learn styling, component API by writing Notes CRUD App
-          </Typography>
-          <Typography variant="caption">{`${new Date()}`}</Typography>
+        <CardContent style={styles.content}>
+          {edit ? (
+            <FormControl fullWidth>
+              <InputLabel htmlFor="note-text">Text</InputLabel>
+              <Input
+                value={text}
+                name="text"
+                id="note-text"
+                onChange={this.handleChange}
+                endAdornment={<Icon>edit_icon</Icon>}
+                fullWidth
+                multiline
+                rowsMax={8}
+              />
+            </FormControl>
+          ) : (
+            <Typography gutterBottom variant="body1" component="p">
+              {text}
+            </Typography>
+          )}
+          <Divider style={{ marginTop: 15, marginBottom: 10 }} />
+          <Typography variant="caption">{updatedAt.toLocaleString()}</Typography>
         </CardContent>
       </Card>
     );
--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,13 +1,52 @@
 import * as React from 'react';
+import { GridList, Grid, Typography } from '@material-ui/core';
 
 import Note from './Note';
 
+const mockNotes = [
+  { id: 1, title: 'Note #1', text: 'Do this and that, so I could get ', updatedAt: new Date() },
+  { id: 2, title: 'Note #2', text: 'That, so I could get those', updatedAt: new Date() },
+  { id: 3, title: 'Note #3', text: 'this, that, so I could get dat', updatedAt: new Date() },
+  { id: 4, title: 'Note #4', text: 'Do this and that, those', updatedAt: new Date() },
+  { id: 5, title: 'Note #5', text: 'Nope,this and that, so get those', updatedAt: new Date() },
+  { id: 6, title: 'Note #6', text: 'Kinda that, so I could get those', updatedAt: new Date() },
+  { id: 7, title: 'Note #7', text: 'Props, and that, so I could get those', updatedAt: new Date() }
+];
+
 class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: mockNotes,
+      loading: false,
+      error: false
+    };
+  }
+
+  deleteNote = idToDelete => {
+    const updatedNotes = this.state.notes.filter(({ id }) => id !== idToDelete);
+    this.setState({ notes: updatedNotes });
+  };
+
   render() {
+    const { notes } = this.state;
+
     return (
       <React.Fragment>
-        <Note />
-        <Note />
+        <Typography variant="headline">Your notes</Typography>
+        <Grid container spacing={8}>
+          {notes.map(note => (
+            <Grid key={note.id} item md={4} xs={12} sm={6}>
+              <Note
+                note={note}
+                deleteNote={() => {
+                  this.deleteNote(note.id);
+                }}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </React.Fragment>
     );
   }
